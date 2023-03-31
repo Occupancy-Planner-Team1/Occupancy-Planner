5f2fa6f955db78a8973ca7db51b998ba3be1bd79
emir1312 <115691707+emir1312@users.noreply.github.com>
2023-03-31 12:18:12 +0200

    Add files via upload
    backend folder

---
+++ backend/src/main/java/com/OccupancyPlanner/OccupancyPlanner/OccupancyPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.OccupancyPlanner.OccupancyPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccupancyPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OccupancyPlannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/OccupancyPlanner/OccupancyPlanner/OccupancyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OccupancyPlanner.OccupancyPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccupancyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

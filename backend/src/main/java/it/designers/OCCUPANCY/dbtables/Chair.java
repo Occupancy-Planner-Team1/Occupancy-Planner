9c90da39c9c9cd1dc9bf143357105274f373c7a8
codergod1337 <73815018+codergod1337@users.noreply.github.com>
2023-05-24 18:45:49 +0200

    safd

--- backend/src/main/java/it/designers/OCCUPANCY/dbtables/Chair.java
+++ backend/src/main/java/it/designers/OCCUPANCY/dbtables/Chair.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Chair {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String tisch;
     private String posx;
